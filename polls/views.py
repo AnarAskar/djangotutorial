c38c0e51cd360282526e3565e6590c3c61da699a
AnarAskar <anaraskar356@gmail.com>
2026-03-18 21:34:37 +0300

    Finished part 5 of the official Django tutorial.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+
 from django.db.models import F
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -12,13 +16,22 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions (not including those set to be published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet. 
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -36,12 +49,9 @@ def vote(request, question_id):
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
-            }
+            },
         )
     else:
         selected_question.votes = F("votes") + 1
         selected_question.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-
-
